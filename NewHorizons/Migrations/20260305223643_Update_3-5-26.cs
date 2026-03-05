c378bdbd088b330afbd4f0a823a6626a29ee6fa9
MattIRL <mlewis.tc@gmail.com>
2026-03-05 17:38:59 -0500

    Updated the ApplicationUser to require Unique DisplayName. Added a new Migration.

---
+++ NewHorizons/Migrations/20260305223643_Update_3-5-26.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewHorizons.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_3526 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
